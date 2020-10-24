c9117bd8e78278aadb852b3138b41dd960c54f99
SergBY <57902822+SergBY@users.noreply.github.com>
2020-10-24 18:34:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let newBrConf = bracketsConfig.join('').split(',').join('');
+    let newStr = str.split('');
+
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr[i + 1] === newBrConf[newBrConf.indexOf(newStr[i]) + 1]) {
+            newStr.splice(i, 2);
+            i = -1;
+            if (newStr.length === 0) {
+                return true;
+            }
+            continue;
+        }
+        continue;
+    }
+    return false;
+};
+
